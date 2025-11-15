a3d31c674384f6b9dc15080bf668a7206f9efb33
leo-a11y46 <leonardo.batiste.estudios@gmail.com>
2025-11-15 10:41:48 +0100

    Ejercicio 304 terminado

--- Ejercicios03/Ejercicios304/Alumno.java
+++ Ejercicios03/Ejercicios304/Alumno.java
@@ -1,7 +1,9 @@
 package Ejercicios03.Ejercicios304;
 
+import javax.sound.sampled.spi.AudioFileWriter;
+
 public class Alumno {
-    //propiedades
+//propiedades
         private String nombre;
 
         private float nota;
@@ -9,14 +11,14 @@ public class Alumno {
         private int edad;
 
     
-    //Constructor
+//Constructor
     public Alumno(String n){
         this.nombre=n;
 
     }
 
 
-    //Metodos
+//Metodos
     void ponNota(int n){
         this.nota=n;
 
@@ -27,15 +29,75 @@ public class Alumno {
           float media=0f;
           int i=0;
           
+
+
           for (i=0;i<array.length;i++){
                media+=array[i].getNota();
           }
           return media/i;
      }
 
-     static
+    static void ordenarArray(Alumno[] array){
+        //Variables declaradas metodo
+        Alumno[] temp=new Alumno[1];
+        
 
-    //Getter
+        for (int i=0;i<array.length;i++){
+
+            for (int x=i+1;x<array.length;x++){
+
+                if ((array[x].getNota())>(array[i].getNota())){ //Ordena nota mayor a menor
+
+                    temp[0]=array[x];
+                    array[x]=array[i];
+                    array[i]=temp[0];
+
+                }
+            }
+        }
+     }
+
+     
+    static void imprimirArray(Alumno[] array){
+    /*
+    * Los array que tienen objetos, hay que tratarlos como static, ya que al contener objetos no puedes manejar solo 1, sino todos los objetos a la vez, osea static=clase
+    */
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i].getNombre());
+        }
+     }
+
+     static void imprimirArrayConNota(Alumno[] array){
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i].getNombre()+": "+array[i].getNota());
+        }
+     }
+
+    
+    static void peorNota(Alumno[] array){
+        //Variables declaradas metodo
+        Alumno[] temp=new Alumno[1];
+        int ultimaPosicionArray=(array.length)-1;
+        
+
+        for (int i=0;i<array.length;i++){
+
+            for (int x=i+1;x<array.length;x++){
+
+                if ((array[x].getNota())>(array[i].getNota())){ //Ordena nota mayor a menor
+
+                    temp[0]=array[x];
+                    array[x]=array[i];
+                    array[i]=temp[0];
+
+                }
+            }
+        }
+        
+        System.out.println(array[ultimaPosicionArray].getNombre()+" "+array[ultimaPosicionArray].getNota());
+    }
+
+//Getter
     public String getNombre(){
         return nombre;
     }
@@ -48,7 +110,7 @@ public class Alumno {
         return edad;
     }
 
-    //setter
+//setter
     public void setNota(float nota){
         this.nota=nota;
     }
--- Ejercicios03/Ejercicios304/PruebaAlumno.java
+++ Ejercicios03/Ejercicios304/PruebaAlumno.java
@@ -2,33 +2,49 @@ package Ejercicios03.Ejercicios304;
 import java.util.*;
 public class PruebaAlumno {
      public static void main(String[] args) {
-          //Declarados en el main
+     //Declarados en el main
           Alumno[] listaAlumnos=new Alumno[5];
+
           Scanner input=new Scanner(System.in);
+
           String nombreAlumno="";
           float notaAlumno=0.0f;
 
-          //Bucle pide nombre 5 alumnos y los añade al array
+
+
+     //Bucle pide nombre 5 alumnos y los añade al array
           for (int i=0;i<listaAlumnos.length;i++){
                System.out.print("Introduce nombre de alumno "+(i+1)+":");
                nombreAlumno=input.nextLine();
                listaAlumnos[i]=new Alumno(nombreAlumno);
           }
+          
+     System.out.println("---------------");
 
-          //Bucle pide 5 notas y los añade al array
+     //Bucle pide 5 notas y los añade al array
           for (int i=0;i<listaAlumnos.length;i++){
-               System.out.println("Introduce la nota del alumno "+listaAlumnos[i]);
+               System.out.print("Introduce la nota del alumno "+listaAlumnos[i].getNombre()+":");
                notaAlumno=(float)input.nextDouble();
                input.nextLine();
 
                listaAlumnos[i].setNota(notaAlumno);
           }
+          
+     System.out.println("---------------");
 
-          //Nota media
-          System.out.println(Alumno.notaMedia(listaAlumnos));
+     //Nota media
+          System.out.println("La nota media de los alumnos es: "+Alumno.notaMedia(listaAlumnos));
 
+     //Ordenar array
+          Alumno.ordenarArray(listaAlumnos);
 
+     //Imprimir array ordenado de mayor a menor
+          System.out.println("Lista alumnos ordenador mayor a menor");
+          Alumno.imprimirArrayConNota(listaAlumnos);
 
+     //Imprimir alumno del array con peor nota
+     System.out.print("Alumno con peor nota es: ");
+     Alumno.peorNota(listaAlumnos);
 
         
      }
